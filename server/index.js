13915fd96ebe252698c0ac7d50588784fc8f934f
Edison Wang <edisonwang03@gmail.com>
2023-09-04 15:00:00 -0400

    Fixed login route and edited user model

--- server/index.js
+++ server/index.js
@@ -47,6 +47,7 @@ const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    dbName: "grapevine",
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 }).catch((error) => console.log(`${error} did not connect`));
